0b109ce82cb18ea33049fe956ef8abc28230d063
rudolfs.streics@gmail.com <rudolfs.streics@gmail.com>
2025-09-09 13:23:06 +0000

    renaming source views to be singular. Renaming columns in source views according to specification

--- models/sources/claim_raw.sql
+++ models/sources/claim_raw.sql
@@ -1,14 +1,12 @@
-/*
-*/
 
 {{ config(materialized='view') }}
 
 with source_data as (
     select 
-    [claim_id]
-    ,[policy_id]
-    ,[claim_date]
-    ,[claim_amount]
+    [claim_id] AS id_claim
+    ,[policy_id] AS id_policy
+    ,[claim_date] AS d_claim
+    ,[claim_amount] AS amt_claim
     from {{ source('insurance_raw','claims') }}
 
 )
